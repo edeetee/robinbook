ee88e5bee2aed5039e00c71a1c47e36ce7a081e7
Edward Taylor <edeetee@gmail.com>
2018-04-07 18:02:40 +1200

    initial commit

---
+++ src.js
@@ -0,0 +1,5 @@
+const $ = require('jquery');
+
+if(document.URL.match("*://*.facebook.com/*")){
+    $("fsl fwb fcb")
+}
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: './src.js',
+  output: {
+    filename: 'bundle.js'
+  }
+};
