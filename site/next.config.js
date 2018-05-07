33967730c181bc06b5c953d7fe6199aeeef01a6a
Edward Taylor <edeetee@gmail.com>
2018-05-07 23:49:37 +1200

    its current

--- site/next.config.js
+++ site/next.config.js
@@ -1,8 +1,19 @@
+const Uglify = 
+
 module.exports = {
     exportPathMap: function () {
         return {
             '/': { page: '/' }
         };
     },
-    assetPrefix: process.env.NODE_ENV == 'production' ? '/robinbook' : ''
+    assetPrefix: process.env.NODE_ENV == 'production' ? '/robinbook' : '',
+    webpack: function (cfg) {
+        cfg.plugins = cfg.plugins.filter(plugin => 
+            plugin.constructor.name !== 'UglifyJsPlugin'
+        );
+
+        // cfg.push(new)
+
+        return cfg
+    }
 }
